05ae70ed491991f8a6ba4248e8e2533c3f5cfb1c
unknown <m13611268495@163.com>
2019-01-23 14:55:51 +0800

    111

--- basic/config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'token'  =>array(
-        'appID'=>"wx7ca5b5e500f6ab73",
-        'appsecret'=>"189a9d0477beac0bd886355c09c0ce53"
-    )
-];
